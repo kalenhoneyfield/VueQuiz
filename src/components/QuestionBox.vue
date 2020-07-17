768750ec35517dd01ed4877220c817731bd7560a
Kalen <kalenhoneyfield@gmail.com>
2020-07-16 20:21:19 -0400

    initial commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>Question goes here?</template>
+
+      <hr class="my-4" />
+
+      <p>List of answers</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
